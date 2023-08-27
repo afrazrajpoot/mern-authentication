6ce9dd66aed6cb9a94052d2f65ed9a376a0daa30
Afraz Rajpoot <afrazrajpoot46@gemail.com>
2023-08-27 16:23:54 -0700

    install react app and tailwind css first template

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="bg-blue-500 px-[2rem]">
+      <h1 className=" ">Hello world</h1>
+    </div>
+  );
+}
+
+export default App
