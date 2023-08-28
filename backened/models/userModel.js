4835d6f57df3a500b174e1b41a5092bf07eb67c4
Afraz Rajpoot <afrazrajpoot46@gemail.com>
2023-08-28 12:32:24 -0700

    add backend

---
+++ backened/models/userModel.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+const scheme = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+const User = mongoose.model('user',scheme)
+module.exports = User
